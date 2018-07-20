1fd4acd7c9b41eea3a6e4eff76e9c42467d48c3c
Alexander Petrovets <apetrovets@sugarcrm.com>
2018-07-20 16:23:01 +0300

    fixed src tag

--- source/components/Profile.js
+++ source/components/Profile.js
@@ -7,7 +7,7 @@ export default class Profile extends React.Component {
     render () {
         return (
             <div className = 'profile'>
-                <img scr = { avatar } />
+                <img src = { avatar } />
             </div>
 
         );
